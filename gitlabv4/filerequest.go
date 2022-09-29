b8c4bee841a58ca516b8e42a649343ececfb204b
Jannek Rodenburger <jannek.rodenburger@vrg.de>
2022-09-29 14:25:51 +0200

    integrated get-request

--- gitlabv4/filerequest.go
+++ gitlabv4/filerequest.go
@@ -1,40 +1,31 @@
 package gitlabv4
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/jnnkrdb/corerdb/fnc"
 	"github.com/jnnkrdb/corerdb/prtcl"
-	"github.com/jnnkrdb/gitlang/f"
 )
 
 // struct to describe a file request for the gtlab v4 api
 type V4Request struct {
-	FilePath   string
-	HTTPMethod string
-	File       FileInformation
-}
-
-// create a new request with predefined variables
-// and a relative file path, which will automatically be url-encoded
-func CreateRequest(file FileInformation, relativepath string) V4Request {
-
-	return V4Request{
-		FilePath:   f.EncodeURL(relativepath),
-		File:       file,
-		HTTPMethod: "POST",
-	}
+	FilePath string
+	File     FileInformation
 }
 
 // check current file state, if the file exists in the specific filepath of the request
 // the request method will be put, to update the file, else its post, to create the file
-func (v4 *V4Request) CheckFile(api string, proj Project) {
+//
+// Parameters:
+//   - `proj` : Project > destination project
+func (v4 *V4Request) checkFile(proj Project) string {
 
-	v4.HTTPMethod = "POST"
+	if request, err := http.NewRequest("GET", proj.BaseURL()+v4.FilePath+"?ref="+v4.File.Branch, nil); err != nil {
 
-	if request, err := http.NewRequest("GET", proj.BaseURL(api)+v4.FilePath+"?ref="+v4.File.Branch, nil); err != nil {
-
-		prtcl.PrintObject(api, v4, proj, request, err)
+		prtcl.PrintObject(v4, proj, request, err)
 
 	} else {
 
@@ -46,16 +37,97 @@ func (v4 *V4Request) CheckFile(api string, proj Project) {
 
 			case 200:
 
-				v4.HTTPMethod = "PUT"
+				return "PUT"
 
 			case 404:
 
-				v4.HTTPMethod = "POST"
+				return "POST"
 
 			default:
 
-				prtcl.PrintObject(v4, api, proj, request, result, err)
+				prtcl.PrintObject(v4, proj, request, result, err)
+
 			}
 		}
 	}
+
+	return "POST"
+}
+
+// send a post or put request
+//
+// Parameters:
+//   - `proj` : Project > destination project
+func (v4 V4Request) Push(proj Project) error {
+
+	req := v4.checkFile(proj)
+
+	if request, err := http.NewRequest(req, proj.BaseURL()+v4.FilePath, bytes.NewReader(v4.File.JSON())); err != nil {
+
+		prtcl.PrintObject(v4, proj, request, err)
+
+		return err
+
+	} else {
+
+		request.Header.Add("PRIVATE-TOKEN", fnc.UnencodeB64(proj.AccessToken))
+
+		request.Header.Add("Content-Type", "application/json")
+
+		if result, err := http.DefaultClient.Do(request); err == nil {
+
+			defer result.Body.Close()
+
+			prtcl.PrintObject(io.ReadAll(result.Body))
+
+			return nil
+
+		} else {
+
+			prtcl.PrintObject(v4, proj, request, result, err)
+
+			return err
+		}
+	}
+}
+
+// receive a file via get
+//
+// Parameters:
+//   - `proj` : Project > destination project
+func (v4 V4Request) Get(proj Project) (gr GetResponse, err error) {
+
+	var request *http.Request
+	if request, err = http.NewRequest("GET", proj.BaseURL()+v4.FilePath+"?ref="+v4.File.Branch, nil); err != nil {
+
+		prtcl.PrintObject(v4, proj, request, gr, err)
+
+	} else {
+
+		request.Header.Add("PRIVATE-TOKEN", fnc.UnencodeB64(proj.AccessToken))
+
+		request.Header.Add("Content-Type", "application/json")
+
+		var result *http.Response
+		if result, err = http.DefaultClient.Do(request); err != nil {
+
+			prtcl.PrintObject(v4, proj, request, result, gr, err)
+
+		} else {
+
+			defer result.Body.Close()
+
+			var bytes []byte
+			if bytes, err = io.ReadAll(result.Body); err != nil {
+
+				prtcl.PrintObject(v4, proj, request, result, bytes, gr, err)
+
+			} else {
+
+				err = json.Unmarshal(bytes, &gr)
+			}
+		}
+	}
+
+	return
 }
--- gitlabv4/project.go
+++ gitlabv4/project.go
@@ -1,14 +1,5 @@
 package gitlabv4
 
-import (
-	"bytes"
-	"io"
-	"net/http"
-
-	"github.com/jnnkrdb/corerdb/fnc"
-	"github.com/jnnkrdb/corerdb/prtcl"
-)
-
 /*
 
 Find ore information about the GitLab API:
@@ -20,6 +11,7 @@ https://docs.gitlab.com/ee/api/personal_access_tokens.html
 
 // project information
 type Project struct {
+	ApiV4       string `json:"apiv4"`
 	AccessToken string `json:"accesstoken"`
 	ID          string `json:"projectid"`
 }
@@ -30,44 +22,7 @@ type Project struct {
 //
 // Parameters:
 //   - `apiv4` : string > api url of the v4 version, something like "https://url.to.gitlab/api/v4/"
-func (p Project) BaseURL(apiv4 string) string {
+func (p Project) BaseURL() string {
 
-	return apiv4 + "/projects/" + p.ID + "/repository/files/"
-}
-
-// send an v4request to the gitlab project, if the returned int value
-// equals "1" there was an error with the request
-//
-// Parameters:
-//   - `api` : string > contains the base v4 path of the api
-//   - `req` : V4Request > request information, send to the gitlab api
-func (p Project) Send(api string, req V4Request) int {
-
-	req.CheckFile(api, p)
-
-	if request, err := http.NewRequest(req.HTTPMethod, p.BaseURL(api)+req.FilePath, bytes.NewReader(req.File.JSON())); err != nil {
-
-		prtcl.PrintObject(api, req, p, request, err)
-
-	} else {
-
-		request.Header.Add("PRIVATE-TOKEN", fnc.UnencodeB64(p.AccessToken))
-
-		request.Header.Add("Content-Type", "application/json")
-
-		if result, err := http.DefaultClient.Do(request); err == nil {
-
-			defer result.Body.Close()
-
-			prtcl.PrintObject(io.ReadAll(result.Body))
-
-			return result.StatusCode
-
-		} else {
-
-			prtcl.PrintObject(api, req, p, request, result, err)
-		}
-	}
-
-	return 1
+	return p.ApiV4 + "/projects/" + p.ID + "/repository/files/"
 }
